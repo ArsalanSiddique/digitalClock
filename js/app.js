ee6c5e5eacf121f12c5fd3af139368af432d2b96
Arsalan <arsalan.ahmed1264@gmail.com>
2019-02-06 01:43:02 +0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+<script>
+
+    function startTime() {
+
+        var dateTime = new Date();
+        var day = dateTime.getDay();
+        var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+        var d = dayNames[day];
+        alert(d);
+
+        
+    }
+
+    
+</script>
